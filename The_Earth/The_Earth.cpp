44183d9788547624e7693cdc9e1b750ae1324de4
MatheusRFC <matheusr220998@gmail.com>
2021-03-05 12:09:33 -0300

    Dependencies installed and the project development will begins.

---
+++ The_Earth/The_Earth.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+#include <glm/glm.hpp>
+#include <glm/gtx/string_cast.hpp>
+#include <stb_image.h>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+using namespace std;
+
+int main()
+{
+	glm::vec3 V{ 1, 2, 3 };
+
+	glfwInit();
+	glewInit();
+
+	cout << glm::to_string(V) << endl;
+	return 0;
+
+}
