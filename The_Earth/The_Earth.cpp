7e5c1c63e2e6282e38633bbaac3e4a1e7caf15ca
MatheusRFC <matheusr220998@gmail.com>
2021-03-05 14:32:08 -0300

    Some functions to check the version of OpenGL, GLSL and GPU Vendor were added.
    Minor fixes.

--- The_Earth/The_Earth.cpp
+++ The_Earth/The_Earth.cpp
@@ -1,5 +1,7 @@
 ﻿#include <iostream>
+#include <locale.h>
 
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
 using namespace std;
@@ -9,7 +11,7 @@ const int altura = 720;
 
 int main()
 {
-
+	setlocale(LC_ALL, "Portuguese");
 	// Inicia a GLFW e verifica caso ocorra algum erro.
 	if (glfwInit() == GLFW_FALSE){
 
@@ -19,8 +21,27 @@ int main()
 	// Criação da janela principal.
 	GLFWwindow* janela = glfwCreateWindow(largura, altura, "The Earth", nullptr, nullptr);
 
-	// Loop de eventos da aplicação.
+	glfwMakeContextCurrent(janela);
 
+	// Inicializa o GLEW
+	if (glewInit() != GLEW_OK) {
+		cout << "Erro ao iniciar o GLEW";
+	}
+
+	//Verifica a versão do OpenGL e obtém algumas informações acerca do driver de vídeo.
+	GLint GLVersaoMaior = 0;
+	GLint GLVersaoMenor = 0;
+
+	glGetIntegerv(GL_MAJOR_VERSION, &GLVersaoMaior);
+	glGetIntegerv(GL_MINOR_VERSION, &GLVersaoMenor);
+
+	cout << "Fabricante da GPU: " << glGetString(GL_VENDOR) << "\n";
+	cout << "Renderizador OpenGL: " << glGetString(GL_RENDERER) << "\n";
+	cout << "Versão do OpenGL: " << glGetString(GL_VERSION) << "\n";
+	cout << "Versão do GLSL: " << glGetString(GL_SHADING_LANGUAGE_VERSION) << "\n";
+
+
+	// Loop de eventos da aplicação.
 	while (!glfwWindowShouldClose(janela)) {
 
 		// Realiza os processamentos em fila do GLFW, e esses eventos podem consistir de teclas apertadas, interação com o mouse etc.
