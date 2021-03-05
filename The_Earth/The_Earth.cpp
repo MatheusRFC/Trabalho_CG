38211adfae4ad0ca25bf79ed0cddf0ffdf87f447
MatheusRFC <matheusr220998@gmail.com>
2021-03-05 13:13:48 -0300

    The window where the project will be shown was created.

--- The_Earth/The_Earth.cpp
+++ The_Earth/The_Earth.cpp
@@ -1,21 +1,39 @@
 ﻿#include <iostream>
 
-#include <glm/glm.hpp>
-#include <glm/gtx/string_cast.hpp>
-#include <stb_image.h>
-#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
 using namespace std;
 
+const int largura = 1280;
+const int altura = 720;
+
 int main()
 {
-	glm::vec3 V{ 1, 2, 3 };
 
-	glfwInit();
-	glewInit();
+	// Inicia a GLFW e verifica caso ocorra algum erro.
+	if (glfwInit() == GLFW_FALSE){
 
-	cout << glm::to_string(V) << endl;
+		cout << "Erro ao iniciar o GLFW";
+	}
+
+	// Criação da janela principal.
+	GLFWwindow* janela = glfwCreateWindow(largura, altura, "The Earth", nullptr, nullptr);
+
+	// Loop de eventos da aplicação.
+
+	while (!glfwWindowShouldClose(janela)) {
+
+		// Realiza os processamentos em fila do GLFW, e esses eventos podem consistir de teclas apertadas, interação com o mouse etc.
+		glfwPollEvents();
+
+		// Envia para ser desenhado na tela o conteúdo do framebuffer.
+		glfwSwapBuffers(janela);
+	}
+
+	// Comando para o encerramento da biblioteca GLFW
+	glfwTerminate();
+
+
 	return 0;
 
 }
