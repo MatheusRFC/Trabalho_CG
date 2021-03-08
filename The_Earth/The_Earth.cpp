eaee12edbb3ce9bfa587e963bae7ccf6ad0d1701
MatheusRFC <matheusr220998@gmail.com>
2021-03-08 19:51:05 -0300

    Some tests to draw a triangle on screen.

--- The_Earth/The_Earth.cpp
+++ The_Earth/The_Earth.cpp
@@ -1,8 +1,9 @@
 ﻿#include <iostream>
 #include <locale.h>
-
+#include <array>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
 
 using namespace std;
 
@@ -40,9 +41,46 @@ int main()
 	cout << "Versão do OpenGL: " << glGetString(GL_VERSION) << "\n";
 	cout << "Versão do GLSL: " << glGetString(GL_SHADING_LANGUAGE_VERSION) << "\n";
 
+	//Definição do triango usando coordenadas normalizadas
+	array < glm::vec3, 3> Triangulo = {
+		glm::vec3{-1.0f, -1.0f, 0.0f},
+		glm::vec3{ 1.0f, -1.0f, 0.0f}, 
+		glm::vec3{ 0.0f,  1.0f, 0.0f}
+	};
+
+	//Copiando os vértices do triangulo para a memória da GPU
+	GLuint Buffer_Vertices;
+
+	//Gera o identificador do buffer de vertices
+	glGenBuffers(1, &Buffer_Vertices);
+
+	//Ativa o buffer de vertices, onde serão copiados os dados do triangulo.
+	glBindBuffer(GL_ARRAY_BUFFER, Buffer_Vertices);
+
+	//Copiar os dados para a memoria de video
+	glBufferData(GL_ARRAY_BUFFER, sizeof(Triangulo), Triangulo.data(), GL_STATIC_DRAW);
+
+	//Define a cor de fundo
+	glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
 
 	// Loop de eventos da aplicação.
 	while (!glfwWindowShouldClose(janela)) {
+
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glEnableVertexAttribArray(0);
+
+		//informa o opengl que o Buffer de Vertices vai ser o ativo no momento.
+		glBindBuffer(GL_ARRAY_BUFFER, Buffer_Vertices);
+
+
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		//Reverter o estado que foi criado
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		glDisableVertexAttribArray(0);
 
 		// Realiza os processamentos em fila do GLFW, e esses eventos podem consistir de teclas apertadas, interação com o mouse etc.
 		glfwPollEvents();
@@ -50,6 +88,9 @@ int main()
 		// Envia para ser desenhado na tela o conteúdo do framebuffer.
 		glfwSwapBuffers(janela);
 	}
+
+	//Desaloca o Buffer de Vertices
+	glDeleteBuffers(1, &Buffer_Vertices);
 
 	// Comando para o encerramento da biblioteca GLFW
 	glfwTerminate();
